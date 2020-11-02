038b73a11df22fd79d35a908ca6647412d0df086
PRASHANTH RAO N <68043931+PRASHANTH337@users.noreply.github.com>
2020-11-02 16:32:54 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,6 +1,8 @@
-#/bin/bash
+#!/bin/bash
 bash /sample-repo-code/collect.sh 
 cd collector
+git config --global user.email "prashanth@gmail.com"
+git config --global user.name "prash"
 mv /powervs* .
 data=$(date '+%B%d')
 git add .
