872dafbeb8ba25d046acb83a2e52e706ab45177e
prash <prashanth@gmail.com>
2020-11-02 13:23:29 +0000

    November02+1

--- sample.sh
+++ sample.sh
@@ -4,6 +4,7 @@ cd collector
 git config --global user.email "prashanth@gmail.com"
 git config --global user.name "prash"
 mv /powervs* .
+ls
 data=$(date '+%B%d')
 git add .
 git commit -m "$data+1"
