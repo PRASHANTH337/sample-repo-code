e38b5cb37d78e10b15f827f0eb0efb7fc118670e
PRASHANTH RAO N <68043931+PRASHANTH337@users.noreply.github.com>
2020-11-09 15:40:54 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-git clone https://github.com/PRASHANTH337/sample-repo-code.git
+wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
+unzip ./terraform_0.12.2_linux_amd64.zip -d /usr/local/bin
+curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+yum install -y jq
+ibmcloud login --apikey <api-key> -o prasraon@in.ibm.com -s dev
 bash /sample-repo-code/collect.sh
-ls
-pwd 
-mv powervs* .
-cd /collector
+RUN git clone git@github.com:PRASHANTH337/collector.git
+cd collector
 git config --global user.email "prashanth@gmail.com"
 git config --global user.name "prash"
+mv /powervs* .
 data=$(date '+%B%d')
 git add .
 git commit -m "$data"
 git push origin master
-git pull
 
 
