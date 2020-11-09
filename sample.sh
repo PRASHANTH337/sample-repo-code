2a24edea2e6566ecbc2b7f5dce4ac1aa958ca33a
PRASHANTH RAO N <68043931+PRASHANTH337@users.noreply.github.com>
2020-11-09 16:25:12 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
+apt install -y unzip
 unzip ./terraform_0.12.2_linux_amd64.zip -d /usr/local/bin
 curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 yum install -y jq
-ibmcloud login --apikey <api-key> -o prasraon@in.ibm.com -s dev
+ibmcloud login --apikey 37nT_NsWWoMXkYTmyD9BPDWcfybY6TUCc-cCweNZtYwj -o prasraon@in.ibm.com -s dev
 bash /sample-repo-code/collect.sh
 RUN git clone git@github.com:PRASHANTH337/collector.git
 cd collector
