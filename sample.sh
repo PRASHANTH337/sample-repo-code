35764b03b67629e80b6345724b62f90f2de71904
PRASHANTH RAO N <68043931+PRASHANTH337@users.noreply.github.com>
2020-11-10 18:03:33 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
+apt-get update -y
+apt-get install -y wget curl git
 wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_darwin_amd64.zip
-sudo apt install -y unzip
+git clone https://github.com/PRASHANTH337/sample-repo-code.git
+apt-get install -y unzip
 unzip ./terraform_0.12.2_darwin_amd64.zip -d /usr/local/bin
 curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-sudo apt install -y jq
-ibmcloud login --apikey 37nT_NsWWoMXkYTmyD9BPDWcfybY6TUCc-cCweNZtYwj -o prasraon@in.ibm.com -s dev
+apt-get install -y jq
+ibmcloud login --apikey 37nT_NsWWoMXkYTmyD9BPDWcfybY6TUCc-cCweNZtYwj 
+ibmcloud target -o prasraon@in.ibm.com -s dev
 bash /sample-repo-code/collect.sh
-RUN git clone git@github.com:PRASHANTH337/collector.git
+git clone https://prashanth337:7b1a12af653fd82d4ebd343eea647a38d8073ed3@github.com/PRASHANTH337/collector.git
 cd collector
 git config --global user.email "prashanth@gmail.com"
 git config --global user.name "prash"
 mv /powervs* .
 data=$(date '+%B%d')
-git add .
-git commit -m "$data"
-git push origin master
-
-
